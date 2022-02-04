edb6a74bd18c706e3c87ea938878109869af49d8
lee <lee@underneath.ca>
2022-02-03 21:59:49 -0800

    vite asks wtr for synthetic web-socket.js file
    enables wtr commands with vite

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ module.exports = function () {
   return {
     name: "vite-plugin",
 
-    async serverStart({ app }) {
+    async serverStart({ app, server, config }) {
+      console.log("app", app);
+      console.log("server", server);
+      console.log("config", config);
       server = await vite.createServer({
         clearScreen: false,
+        resolve: {
+          alias: [
+            {
+              find: "/__web-dev-server__web-socket.js",
+              replacement:
+                "http://localhost:8000/__web-dev-server__web-socket.js",
+            },
+          ],
+        },
       });
       await server.listen();
       const port = server.config.server.port;
