6bbef2ba9b9374cdcef73af78aa7a0b2784066da
lee <lee@underneath.ca>
2022-02-20 18:50:45 -0800

    feat: vite asks wtr for synthetic web-socket.js file
    enables using web-test-runner commands with vite
    
    refactor: wds server path detection more general
    
    feat: test for web test runner prefix too

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const vite = require("vite");
 
+const WDS_FILE_PREFIX = "__web-dev-server__";
+const WTR_FILE_PREFIX = "__web-test-runner__";
+
 module.exports = function () {
   let server;
 
   return {
     name: "vite-plugin",
 
-    async serverStart({ app }) {
+    async serverStart({ app, server, config }) {
       server = await vite.createServer({
         clearScreen: false,
+        resolve: {
+          alias: [
+            {
+              find: new RegExp(`\/(${WDS_FILE_PREFIX}|${WTR_FILE_PREFIX}.*)`),
+              replacement: `http://${config.hostname}:${config.port}/$1`,
+            },
+          ],
+        },
       });
       await server.listen();
       const port = server.config.server.port;
