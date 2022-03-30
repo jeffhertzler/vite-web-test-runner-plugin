cbee2cadb6b2d7d7fcf25ab6aee9a92be11ae9a0
lee <lee@underneath.ca>
2022-03-29 20:44:24 -0700

    refactor: comment and clarify a bit

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const WDS_FILE_PREFIX = "__web-dev-server__";
 const WTR_FILE_PREFIX = "__web-test-runner__";
 
 module.exports = function () {
-  let server;
+  let viteServer;
 
   return {
     name: "vite-plugin",
 
-    async serverStart({ app, server, config }) {
-      server = await vite.createServer({
+    async serverStart({ app, config }) {
+      viteServer = await vite.createServer({
         clearScreen: false,
         resolve: {
+          // if vite sees an import for a wtr generated path, fetch the path from wtr
           alias: [
             {
               find: new RegExp(`\/(${WDS_FILE_PREFIX}|${WTR_FILE_PREFIX}.*)`),
@@ -21,17 +22,22 @@ module.exports = function () {
           ],
         },
       });
-      await server.listen();
-      const port = server.config.server.port;
-      const protocol = server.config.server.https ? "https" : "http";
+      await viteServer.listen();
+
+      const port = viteServer.config.server.port;
+      const protocol = viteServer.config.server.https ? "https" : "http";
       app.use((ctx, next) => {
-        ctx.redirect(`${protocol}://localhost:${port}${ctx.originalUrl}`);
-        return;
+        const { path, querystring } = ctx;
+        // redirect all requests except those for wtr generated files to the vite server
+        const querySep = querystring?.length ? "?" : "";
+        const viteUrl = `${protocol}://localhost:${port}${path}${querySep}${querystring}`;
+        ctx.redirect(viteUrl);
+        next();
       });
     },
 
     async serverStop() {
-      return server.close();
+      return viteServer.close();
     },
   };
 };
